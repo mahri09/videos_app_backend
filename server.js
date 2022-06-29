b12631a17ab901fa8e5103b86d36d3fd9e1ab7f9
mahri09 <mahriaa88@gmail.com>
2022-06-30 00:48:29 +0100

    created server for videos app

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const cors = require("cors");
+const videos = require("./routes/videos");
+
+
+const app = express();
+
+
+const port = process.env.PORT || 3000;
+app.use(cors());
+
+app.use(express.json());
+
+app.use(express.urlencoded({ extended: false }));
+
+// GET "/"
+app.get("/", (req, res) => {
+  // Delete this line after you've confirmed your server is running
+  res.send({ express: "Your Backend Service is Running" });
+});
+
+app.use("/videos", videos);
+
+
+
+
+
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
